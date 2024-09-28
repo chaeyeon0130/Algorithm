bff0f74363e5761184f21c37cb19c3d78e676c9a
chaeyeon0130 <k84292330@gmail.com>
2024-09-28 23:59:37 +0900

    240928 사탕 게임

--- python/baekjoon/3085.py
+++ python/baekjoon/3085.py
@@ -4,55 +4,52 @@ input = sys.stdin.readline
 n = int(input())
 a = [list(input().strip()) for _ in range(n)]
 
-ans = 0
-
-def check_row(c):
-    compar = a[c][0]
+def check_row(r):
+    max_count = 1
     count = 1
-    global ans
     for i in range(1, n):
-        if compar == a[c][i]:
+        if a[r][i] == a[r][i - 1]:
             count += 1
         else:
-            ans = max(ans, count)
-            compar = a[c][i]
             count = 1
-    ans = max(ans, count)
+        if max_count < count:
+            max_count = count
+    return max_count
             
 def check_col(c):
-    compar = a[0][c]
+    max_count = 1
     count = 1
-    global ans
     for i in range(1, n):
-        if compar == a[i][c]:
+        if a[i][c] == a[i - 1][c]:
             count += 1
         else:
-            ans = max(ans, count)
-            compar = a[i][c]
             count = 1
-    ans = max(ans, count)
+        if max_count < count:
+            max_count = count
+    return max_count
     
 for i in range(n):
     check_row(i)
     
 for i in range(n):
     check_col(i)
-    
+
+ans = 0
 for i in range(n):
     for j in range(n):
         if j != n - 1:
             if a[i][j] != a[i][j + 1]:
                 a[i][j], a[i][j + 1] = a[i][j + 1], a[i][j]
-                check_row(i)
-                check_col(j)
-                check_col(j + 1)
+                ans = max(ans, check_row(i))
+                ans = max(ans, check_col(j))
+                ans = max(ans, check_col(j + 1))
                 a[i][j], a[i][j + 1] = a[i][j + 1], a[i][j]
         if i != n - 1:
             if a[i][j] != a[i + 1][j]:
                 a[i][j], a[i + 1][j] = a[i + 1][j], a[i][j]
-                check_row(i)
-                check_row(i + 1)
-                check_col(j)
+                ans = max(ans, check_row(i))
+                ans = max(ans, check_row(i + 1))
+                ans = max(ans, check_col(j))
                 a[i][j], a[i + 1][j] = a[i + 1][j], a[i][j]
 
 print(ans)
