bb3a247374b51b667108d1562c38c26710ecb937
chaeyeon0130 <k84292330@gmail.com>
2024-12-16 01:38:33 +0900

    241216 2*n 타일링

---
+++ python/baekjoon/11726.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+d = [0] * 1001
+d[0] = d[1] = 1
+
+n = int(input())
+
+for i in range(2, n + 1):
+    d[i] = d[i - 1] + d[i - 2]
+    d[i] %= 10007
+print(d[n])
