58b93fa3a608cc50ce548e33849927fa36db072d
chaeyeon0130 <k84292330@gmail.com>
2024-12-16 02:44:07 +0900

    241216 카드 구매하기2

--- python/baekjoon/11052.py
+++ python/baekjoon/11052.py
@@ -3,11 +3,12 @@ import sys
 input = sys.stdin.readline
 
 # 인덱스는 카드 개수를 의미함
-d = [0] * 1001
+d = [10000000] * 1001
+d[0] = 0
 
 n = int(input())
 p = list(map(int, input().split()))
 for i in range(1, n + 1):
     for j in range(1, i + 1):
-        d[i] = max(d[i], d[i - j] + p[j - 1])
+        d[i] = min(d[i], d[i - j] + p[j - 1])
 print(d[n])
