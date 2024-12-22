41bbdf544386ad16eac4bd794bf6ea6848dcfa73
chaeyeon0130 <k84292330@gmail.com>
2024-12-22 21:21:49 +0900

    241222 가장 긴 바이토닉 부분 수열

--- python/baekjoon/11054.py
+++ python/baekjoon/11054.py
@@ -10,15 +10,14 @@ RLIS = [1] * n
 LBS = [0] * n
 for i in range(n):
     for j in range(i):
-        if a[j] < a[i] and LIS[j] + 1 > LIS[i]:
+        if a[i] > a[j] and LIS[j] + 1 > LIS[i]:
             LIS[i] = LIS[j] + 1
 
 for i in reversed(range(n)):
-    for j in range(i + 1, n):
-        if a[j] < a[i] and RLIS[j] + 1 > RLIS[i]:
+    for j in range(i, n):
+        if a[i] > a[j] and RLIS[j] + 1 > RLIS[i]:
             RLIS[i] = RLIS[j] + 1
 
 for i in range(n):
     LBS[i] = LIS[i] + RLIS[i] - 1
-
 print(max(LBS))
