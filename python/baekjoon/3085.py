a73858badd6c037ed497597349fdf2c3475e3412
chaeyeon0130 <k84292330@gmail.com>
2024-12-23 20:56:37 +0900

    241223 사탕 게임

--- python/baekjoon/3085.py
+++ python/baekjoon/3085.py
@@ -1,55 +1,54 @@
 import sys
 
 input = sys.stdin.readline
+
 n = int(input())
 a = [list(input().strip()) for _ in range(n)]
+result = 0
 
-def check_row(r):
+def check_row(row):
+    max_list = []
     max_count = 1
-    count = 1
+    comp = a[row][0]
     for i in range(1, n):
-        if a[r][i] == a[r][i - 1]:
-            count += 1
+        if comp == a[row][i]:
+            max_count += 1
         else:
-            count = 1
-        if max_count < count:
-            max_count = count
-    return max_count
-            
-def check_col(c):
+            max_list.append(max_count)
+            max_count = 1
+            comp = a[row][i]
+    max_list.append(max_count)
+    return max(max_list)
+
+def check_col(col):
+    max_list = []
     max_count = 1
-    count = 1
+    comp = a[0][col]
     for i in range(1, n):
-        if a[i][c] == a[i - 1][c]:
-            count += 1
+        if comp == a[i][col]:
+            max_count += 1
         else:
-            count = 1
-        if max_count < count:
-            max_count = count
-    return max_count
-    
-for i in range(n):
-    check_row(i)
-    
-for i in range(n):
-    check_col(i)
+            max_list.append(max_count)
+            max_count = 1
+            comp = a[i][col]
+    max_list.append(max_count)
+    return max(max_list)
 
-ans = 0
 for i in range(n):
-    for j in range(n):
-        if j != n - 1:
-            if a[i][j] != a[i][j + 1]:
-                a[i][j], a[i][j + 1] = a[i][j + 1], a[i][j]
-                ans = max(ans, check_row(i))
-                ans = max(ans, check_col(j))
-                ans = max(ans, check_col(j + 1))
-                a[i][j], a[i][j + 1] = a[i][j + 1], a[i][j]
-        if i != n - 1:
-            if a[i][j] != a[i + 1][j]:
-                a[i][j], a[i + 1][j] = a[i + 1][j], a[i][j]
-                ans = max(ans, check_row(i))
-                ans = max(ans, check_row(i + 1))
-                ans = max(ans, check_col(j))
-                a[i][j], a[i + 1][j] = a[i + 1][j], a[i][j]
+    result = max(result, check_row(i))
 
-print(ans)
+for i in range(n):
+    result = max(result, check_col(i))
+
+for i in range(n):
+    for j in range(1, n):
+        a[i][j - 1], a[i][j] = a[i][j], a[i][j - 1]
+        result = max(result, check_row(i), check_col(j), check_col(j -1))
+        a[i][j - 1], a[i][j] = a[i][j], a[i][j - 1]
+
+for i in range(n):
+    for j in range(1, n):
+        a[j - 1][i], a[j][i] = a[j][i], a[j - 1][i]
+        result = max(result, check_col(i), check_row(j), check_row(j - 1))
+        a[j - 1][i], a[j][i] = a[j][i], a[j - 1][i]
+print(result)
