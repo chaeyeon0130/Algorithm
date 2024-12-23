8ea1c100915853aef78d648f12af45e69564f6e2
chaeyeon0130 <k84292330@gmail.com>
2024-12-23 22:59:41 +0900

    241223 리모컨

--- python/baekjoon/1107.py
+++ python/baekjoon/1107.py
@@ -4,29 +4,16 @@ input = sys.stdin.readline
 
 n = int(input())
 m = int(input())
-if m > 0:
-    a = list(map(int, input().split()))
+if m:
+    broken = input().split()
 else:
-    a = []
+    broken = []
 
-broken = [0] * 10
-for i in a:
-    broken[i] = 1
-
-def possible(c):
-    if c == 0:
-        return not broken[0]
-    len = 0
-    while c > 0:
-        if broken[c % 10]:
-            return 0
-        len += 1
-        c //= 10
-    return len
-
-ans = abs(n - 100)
-for c in range(1000001):
-    len = possible(c)
-    if len:
-        ans = min(ans, abs(n - c) + len)
+ans = abs(100 - n)
+for i in range(1000001):
+    for j in str(i):
+        if j in broken:
+            break
+    else:
+        ans = min(ans, len(str(i)) + abs(i - n))
 print(ans)
